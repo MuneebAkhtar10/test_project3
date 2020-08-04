d711561c1fc5f421b44df127de6d416ba45ab1c1
Muneeb Akhtar <m.muneebakhtar1998@gmail.com>
2020-08-04 20:52:05 +0500

    deploy

--- db/migrate/20200728184619_remove_type_columns_from_users.rb
+++ db/migrate/20200728184619_remove_type_columns_from_users.rb
@@ -1,7 +1,7 @@
 class RemoveTypeColumnsFromUsers < ActiveRecord::Migration[6.0]
   def change
-  	remove_column :users, :qa
-  	remove_column :users, :projectmanager
-  	remove_column :users, :developer
+  	# remove_column :users, :qa
+  	# remove_column :users, :projectmanager
+  	# remove_column :users, :developer
   end
 end
