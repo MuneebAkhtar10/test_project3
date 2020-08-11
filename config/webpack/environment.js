d65a46d13dc396b96fa5bfbbabe441a20dd0e6c6
Muneeb Akhtar <m.muneebakhtar1998@gmail.com>
2020-08-11 14:57:18 +0500

    time picker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -9,11 +9,11 @@ environment.plugins.prepend('Provide',
 	})
 	);
 
-const aliasConfig = {
+// const aliasConfig = {
 
-	'jquery': 'jquery-ui-dist/external/jquery/jquery.js',
-		'jquery-ui': 'jquery-ui-dist/jquery.ui.js'
+// 	'jquery': 'jquery-ui-dist/external/jquery/jquery.js',
+// 		'jquery-ui': 'jquery-ui-dist/jquery.ui.js'
 
-};
-environment.config.set('resolve.alias'aliasConfig);
+// };
+// environment.config.set('resolve.alias'aliasConfig);
 module.exports = environment
